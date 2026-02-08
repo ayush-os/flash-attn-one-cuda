6d1b765e793884364e2d0e43e88c755b6de767a2
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-08 12:22:39 -0800

    launch

--- src/flash_attn/flash_attn_kernel.cu
+++ src/flash_attn/flash_attn_kernel.cu
@@ -44,12 +44,25 @@ torch::Tensor flash_attn_cuda_forward(torch::Tensor q, torch::Tensor k, torch::T
     dim3 grid(B, nh);
     dim3 block(Br);
 
-    // TODO: Launch your kernel
-    // flash_attn_kernel<<<grid, block, shared_mem_size>>>(
-    //     q.data_ptr<float>(), k.data_ptr<float>(), v.data_ptr<float>(),
-    //     out.data_ptr<float>(), l.data_ptr<float>(), m.data_ptr<float>(),
-    //     N, d, Bc, Br
-    // );
+    // Size in bytes for dynamic shared memory
+    // Q_tile (Br * d) + K_tile (Bc * d) + V_tile (Bc * d)
+    // O, l, m will be stored in regs
+    size_t shared_mem_bytes = (Br * d + 2 * Bc * d) * sizeof(float);
+
+    flash_attn_kernel<<<grid, block, shared_mem_bytes>>>(
+        q.data_ptr<float>(),
+        k.data_ptr<float>(),
+        v.data_ptr<float>(),
+        out.data_ptr<float>(),
+        l.data_ptr<float>(),
+        m.data_ptr<float>(),
+        B,
+        nh,
+        N,
+        d,
+        Bc,
+        Br,
+        softmax_scale);
 
     return out;
 }
